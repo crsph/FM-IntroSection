b04a90d56c7072105f57e13bc76dedf782dbda34
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-21 21:55:56 +0200

    Added buttons

---
+++ src/components/Buttons.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Buttons.scss";
+
+function LoginButton() {
+  return <button className="login-button">Login</button>;
+}
+
+function RegisterButton() {
+  return <button className="register-button">Register</button>;
+}
+
+export { LoginButton, RegisterButton };
+
